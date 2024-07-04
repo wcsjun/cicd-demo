42374c474eaf3f7e5c8da6d2a7a420f171440b38
wcsjun <1125013028@qq.com>
2024-07-04 20:14:48 +0800

    init project

---
+++ src/main/java/com/jun/codedemo/CodeDemoApplication.java
@@ -0,0 +1,16 @@
+package com.jun.codedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jun
+ */
+@SpringBootApplication
+public class CodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jun/codedemo/CodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.codedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
